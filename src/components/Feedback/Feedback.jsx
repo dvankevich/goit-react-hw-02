13e3a0cb0a539f9c440ed5af50771a6f3232b75d
dvankevich <dvankevich@gmail.com>
2024-10-26 20:21:36 +0300

    add Positive

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,16 +1,20 @@
-import c from "./Feedback.module.css"
+import c from './Feedback.module.css';
 
 const Feedback = ({ values }) => {
+  const total = values.bad + values.good + values.neutral;
   return (
     <div className={c.feedback}>
       <ul className={c.feedbackList}>
         <li className={c.feedbackListItem}>Good: {values.good}</li>
         <li className={c.feedbackListItem}>Neutral: {values.neutral}</li>
         <li className={c.feedbackListItem}>Bad: {values.bad}</li>
-        <li className={c.feedbackListItem}>Total: {values.bad + values.good + values.neutral}</li>
+        <li className={c.feedbackListItem}>Total: {total}</li>
+        <li className={c.feedbackListItem}>
+          Positive: {Math.round((values.good / total) * 100)}%
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Feedback
+export default Feedback;
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,7 @@
+import c from './Notification.module.css';
+
+const Notification = () => {
+  return <div className={c.notification}>No feedback yet</div>;
+};
+
+export default Notification;
